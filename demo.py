7c1dd8ad4b4369a7af60c84eae9d6ea74569bbaf
nishia1 <agrawalnishi007@gmail.com>
2025-08-30 23:52:21 -0400

    writing image to output.jpg

--- demo.py
+++ demo.py
@@ -3,11 +3,20 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
+# read image & write image
+
 def readImage():
     sunset = cv.imread('sunset.jpg')
     breakpoint()
     cv.imshow('sunset', sunset)
     cv.waitKey(0)
 
+def writeImage():
+    img = cv.imread("sunset.jpg")
+    outPath = "output.jpg"
+    cv.imwrite(outPath, img)
+
+
 if __name__ == '__main__':
-    readImage()
+    # readImage()
+    writeImage()
